15045bbfec4c52262bcb8e8a0a5995ae06c0a03e
Avi Saranga <avi@telmate.com>
2019-11-12 13:30:59 -0800

    add pam_conv_h

--- programs/pluto/state.c
+++ programs/pluto/state.c
@@ -84,7 +84,7 @@
 #include "pluto_stats.h"
 #include "ikev2_ipseckey.h"
 #include "ip_address.h"
-
+#include "pam_conv.h"
 static void update_state_stats(struct state *st, enum state_kind old_state,
 			       enum state_kind new_state);
 
